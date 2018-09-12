33548b5c5c27bfc11e45c7b13d012a941837970d
Styopa Semenukha <ssemenukha@developmentgateway.org>
2018-09-12 11:34:38 -0400

    Fixed algorithm bugs; removed PHP parts

--- escape.c
+++ escape.c
@@ -11,6 +11,7 @@ Copyright:
 char *ldap_escape_filter(const char *string) {
 	const char unsafe[] = "\\*()\0";
 	const char escaped[] = "5c2a282900";
+	char *result;
 	/* for the result we need at least as many bytes as the source string plus terminator */
 	int len, new_len;
 	len = new_len = strlen(string);
@@ -34,11 +35,11 @@ char *ldap_escape_filter(const char *string) {
 	int pos = 0;
 	for (int i = 0; i < len; i++) {
 		/* copy each character, even if it's unsafe */
-		result[pos] = string[pos];
+		result[pos] = string[i];
 		for (int j = 0; j < sizeof(unsafe); j++) {
 			if (unsafe[j] == string[i]) {
 				result[pos] = '\\';
-				memcpy(result[pos + 1], escaped[j * 2], 2);
+				memcpy(result + pos + 1, escaped + j * 2, 2);
 				pos += 2;
 				break;
 			}
